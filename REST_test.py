9f48c92d37046d3e7a6f741afbab185337a329fb
denisab85 <denisab85@gmail.com>
2017-06-12 12:42:28 -0500

    Re-done to take test cases from a json file

--- REST_test.py
+++ REST_test.py
@@ -1,87 +1,7 @@
+import re
 import requests
-from xml.etree import ElementTree
 import json
-
-
-def _assert(name, condition, value):
-    out = str(name) + ": "
-    if condition:
-        result = "Pass"
-    else:
-        result = "Fail"
-    out += result
-    out = out + " [" + str(value)
-    print(out + "]")
-    if result == "Fail":
-        exit(1)
-
-
-def start_project(appliance_ip, automation_zip, run_id=""):
-    # upload and run new project as is
-    uri = "http://" + appliance_ip + "/api/run"
-    if len(run_id) != 0:
-        uri = uri + "/" + run_id
-
-    # octet-stream upload
-    data = open(automation_zip, 'rb').read()
-    response = requests.post(uri, data=data, headers={'Content-Type': 'application/octet-stream'})
-
-    # multipart upload
-    # files = {'file': open(automation_zip, 'rb')}
-    # response = requests.post(uri, files=files)
-
-    status_code = int(response.status_code)
-    try:
-        content = response.json()
-    except ValueError or json.decoder.JSONDecodeError:
-        content = "JSON format error"
-    return status_code, content
-
-
-def stop(appliance_ip, kind, id):
-    uri = "http://" + str(appliance_ip) + "/api/" + str(kind) + "/" + str(id) + "/stop"
-    response = requests.put(uri)
-    status_code = int(response.status_code)
-    try:
-        content = response.json()
-    except ValueError or json.decoder.JSONDecodeError:
-        content = "JSON format error"
-    return status_code, content
-
-
-def get_status(appliance_ip, kind, id=""):
-    """
-    kind = Get status for port(s)
-    Get list of all runIds with basic metadata
-    """
-    uri = "http://" + appliance_ip + "/api/" + kind
-    if len(id):
-        uri = uri + "/" + id
-    response = requests.get(uri)
-    status_code = int(response.status_code)
-    try:
-        content = response.json()
-    except ValueError or json.decoder.JSONDecodeError:
-        content = "JSON format error"
-    return status_code, content
-
-
-def check_ports(appliance_ip, port_nums, expected_status):
-    status, content = get_status(appliance_ip, "port")
-    _assert("Status", status == 200, status)
-    if status == 200:
-        _assert("Content", isinstance(content, dict), type(content))
-        if isinstance(content, dict):
-            for port_num in port_nums:
-                result = False
-                status = "not found"
-                for port in content["ports"]:
-                    status = port['status']
-                    if port['port_id'] == port_num and status == expected_status:
-                        result = True
-                _assert(port_num, result, status)
-            # singlePort = get_status(appliance_ip, "port", portNum)
-            # _assert(ports[portNum].portid, (ports[portNum].status == singlePort.status), singlePort.status)
+from xml.etree import ElementTree
 
 
 def get_app_info(appliance_ip):
@@ -93,15 +13,13 @@ def get_app_info(appliance_ip):
     return ver[0].text
 
 
-# ======================================================================================= #
-
-
 # test_ip = "172.17.1.54"
 # test_ip = "192.168.8.104"
-test_ip = "192.168.8.209"
+# test_ip = "192.168.8.209"
 # test_ip = "localhost:8104"
-test_ports = 0, 1
-project_file = "Automation.zip"
+test_ip = "localhost:8209"
+
+project_file = "./Data/Automation.zip"
 project_fileWithPortMismatch = "Automation-port-0-4.zip"
 run_id = "test-project-0001"
 
@@ -109,55 +27,59 @@ run_id = "test-project-0001"
 ver = get_app_info(test_ip)
 print("Backend version: " + ver)
 
-print("\nStop all ports used in this project")
-# stop all ports used in this project
-for portNum in test_ports:
-    status, content = stop(test_ip, "port", portNum)
-    _assert("Status", status == 200, status)
-    if status == 200:
-        _assert("Content", isinstance(content, dict), type(content))
-        if isinstance(content, dict):
-            _assert(portNum, (content["portId"] == portNum and content["status"] == "idle"), content)
+with open('Tests.json') as test_file:
+    tests = json.load(test_file)
 
-print("\nCheck that all ports used in this project are idle")
-# check that all ports used in this project are idle 
-check_ports(test_ip, test_ports, "idle")
+for test in tests:
+    method = test["Method"]
+    uri = test["URI"]
+    uri_list = []
+    content_type = 'text/plain'
+    data = None
+    files = None
 
-print("\nPOST /api/run/{run_id} upload and run new project as is, with specified {run_id}")
-status, content = start_project(test_ip, project_file, run_id)
-_assert("Status", status == 200, status)
-if status == 200:
-    _assert("Content", isinstance(content, dict), type(content))
-    if isinstance(content, dict):
-        _assert("runId", content["runId"] == run_id, content["runId"])
+    print('\n>>>>>>  ', test['Name'], '  <<<<<<')
 
-print("\nCheck that all ports used in this project are busy")
-# check that all ports used in this project are busy
-check_ports(test_ip, test_ports, "busy")
+    content = test.get('Content')
+    if content:
+        file_path = content['data']
+        content_type = content['type']
+        if content_type == 'application/octet-stream':
+            data = open(file_path, 'rb').read()
+        elif content_type == 'multipart/form-data':
+            files = {'file': open(file_path, 'rb')}
 
-print("\nCheck run status")
-status, content = get_status(test_ip, "run", run_id)
-_assert("Status", status == 200, status)
-if status == 200:
-    _assert("Content", isinstance(content, dict), type(content))
-    if isinstance(content, dict):
-        _assert("runId", content["runId"] == run_id, content)
+    if not re.search("({\w+})", uri):
+        uri_list.append(uri)
 
-print("\nGet list of all uploaded projects from appliance")
-status, content = get_status(test_ip, "project")
-_assert("Status", status == 200, status)
-if status == 200:
-    _assert("Content", isinstance(content, dict), type(content))
-    if isinstance(content, dict):
-        for project in content["projects"]:
-            status, single_project = get_status(test_ip, "project", project["projectId"])
-            _assert("Status", status == 200, status)
-            if status == 200:
-                _assert(project["projectId"], single_project == project, "Equal")
+    for parameter in re.findall("({\w+})", uri):
+        param_name = str(parameter[1:-1])
+        param_value = test['Parameters'][param_name]
+        if isinstance(param_value, list):
+            for value in param_value:
+                uri_list.append(uri.replace(parameter, str(value)))
+        else:
+            uri_list.append(uri.replace(parameter, param_value))
 
-print("\nGet list of all runIds with basic metadata")
-status, content = get_status(test_ip, "run")
-if status == 200:
-    _assert("Content", isinstance(content, dict), type(content))
-    if isinstance(content, dict):
-        print(content)
+    for uri in uri_list:
+        uri = "http://" + test_ip + uri
+        print(method, uri)
+        if method == "PUT":
+            response = requests.put(uri, data=data, files=files, headers={'Content-Type': content_type})
+        elif method == "GET":
+            response = requests.get(uri, data=data, files=files, headers={'Content-Type': content_type})
+        elif method == "POST":
+            response = requests.post(uri, data=data, files=files, headers={'Content-Type': content_type})
+
+        status_code = int(response.status_code)
+        try:
+            content = response.json()
+        except ValueError or json.decoder.JSONDecodeError:
+            content = "JSON format error"
+
+        if status_code == 200:
+            print(status_code, 'OK')
+        elif status_code == 404:
+            print(status_code, response.content)
+        else:
+            print(status_code, content['errors'])
